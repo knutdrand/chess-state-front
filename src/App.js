9c812e71d0b05287c9bca6cdeb297c41557be647
Knut Rand <knutdrand@gmail.com>
2024-04-14 10:06:22 +0200

    feature: clicktomove

--- src/App.js
+++ src/App.js
@@ -10,16 +10,15 @@ function ChessApp() {
   const [stateColor, setStateColor] = useState('black');
   const [orientation, setOrientation] = useState('white'); // ['white', 'black'
   const [mode, setMode] = useState('play'); // ['play', 'show', 'repeat'];
+    const [selectedSquare, setSelectedSquare] = useState(null); // [null, 'a2'
     const [whiteScore, setWhiteScore] = useState(0);
     const [blackScore, setBlackScore] = useState(0);
-    //const [playerName, setPlayerName] = useState('kuppern87'); // Declare a state variable...
     const [feedback, setFeedback] = useState(''); // ['play', 'show', 'repeat'
     function getResponse(fen, sourceSquare, targetSquare) {
         const baseUrl = 'https://chess-state.vercel.app';
         //const baseUrl = 'http:///0.0.0.0:8000';
         const urlifiedFen = fen.replace(/ /g, "_").replace(/\//g, '+');
         const url = baseUrl + '/move/' + playerName + '/' + mode + '/' + urlifiedFen + '/' + sourceSquare + '/' + targetSquare + '/';
-        //const url = 'http:///0.0.0.0:8000/move/' + playerName+ '/' + mode + '/' + urlifiedFen + '/' + sourceSquare + '/' + targetSquare + '/';
         axios.post(url).then(
             (response) => {
             console.log(response.data);
@@ -41,25 +40,52 @@ function ChessApp() {
 
   async function onDrop(sourceSquare, targetSquare) {
         const fen = game.fen();
-    // see if the move is legal
       try {
-          game.move({from: sourceSquare, to: targetSquare});
+          const r  = game.move({from: sourceSquare, to: targetSquare});
+          if (!r) return false;
       }
         catch (e) {
           return false;
         }
-    //game.move({from: sourceSquare, to: targetSquare});
+
     setGame(new Chess(game.fen()));
       return getResponse(fen, sourceSquare, targetSquare)
     //return setTimeout(() => getResponse(fen, sourceSquare, targetSquare), 200);
   }
+
+  async function handleSquareClick(square) {
+        if (selectedSquare) {
+            onDrop(selectedSquare, square)
+            setSelectedSquare(null);
+            //try{
+            //    game.move({from: selectedSquare, to: square});
+            //}
+            //catch (e) {
+            //    setSelectedSquare(null);
+            //    return false;
+            //}
+            //setGame(new Chess(game.fen()));
+            //getResponse(game.fen(), selectedSquare, square);
+            //setSelectedSquare(null);
+            }
+        else {
+            setSelectedSquare(square);
+        }
+        return true
+        }
 
   return (
       <div className="ChessState" style={{
         maxWidth: 300,
         maxHeight: 300,
         flexGrow: 1}}>
-          <Chessboard position={game.fen()} onPieceDrop={onDrop} boardOrientation={orientation}/>
+          <Chessboard
+              position={game.fen()}
+              onPieceDrop={onDrop}
+              onSquareClick={handleSquareClick}
+              boardOrientation={orientation}
+              customSquareStyles={selectedSquare ? { [selectedSquare]: { backgroundColor: 'rgba(255, 255, 0, 0.4)' } } : {}}
+          />
           <div Status style={{color: stateColor}} >{mode}: {feedback}, sW: {whiteScore.toFixed(2)}, sB: {blackScore.toFixed(2)}</div>
 
       </div>);
