875b8698257445d02eaee9a25b9eba1684b145b8
Knut Rand <knutdrand@gmail.com>
2024-12-15 15:55:29 +0100

    local

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 const config = {
     apiUrl: 'https://polarchess.com/api',
     localApiUrl: 'http://localhost:8000/api',
-    is_local: true,
+    is_local: false,
 };
 const apiUrl = config.is_local ? config.localApiUrl : config.apiUrl;
 module.exports = { config, apiUrl };
